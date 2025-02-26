d5cc52bc153b13f62f717f36684b5f5b77afe6b8
enigmarikki <hrishi98.m@gmail.com>
2025-02-26 00:17:04 +0000

    "chore : seperate stream requests with non stream requests, todo : flatten data and add human readable"

--- grpc/src/main.rs
+++ grpc/src/main.rs
@@ -1,69 +1,22 @@
-//use chrono::Utc;
-//use proto::injective::oracle::v1beta1::ProviderInfo;
-//use std::sync::Arc;
-//use tokio::sync::mpsc;
-use tonic::Request;//, Response, Status};
 use tokio_stream::StreamExt;
-//use uuid::Uuid;
-//use crate::producer::MessageProducer;
-// Here we get the data and put it in a message queue
-// Why do i need mpsc - idk yet
+use tonic::Request;
 mod proto;
-use crate::proto::injective::stream::v1beta1::{
-    stream_client::StreamClient, BankBalancesFilter, OraclePriceFilter, OrderbookFilter,
-    OrdersFilter, PositionsFilter, StreamRequest, SubaccountDepositsFilter,
-    TradesFilter,
-};
+mod stream;
+use proto::injective::stream::v1beta1::stream_client::StreamClient;
+use stream::{create_stream_request, StreamFilterCondition};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = StreamClient::connect("http://10.100.40.106:1900").await?;
+    let mut client = StreamClient::connect("http://localhost:1999").await?;
     //stream entire state
-    let req = Request::new(StreamRequest {
-        bank_balances_filter: Some(BankBalancesFilter {
-            accounts: vec!["*".to_string()],
-        }),
-        subaccount_deposits_filter: Some(SubaccountDepositsFilter {
-            subaccount_ids: vec!["*".to_string()],
-        }),
-        spot_orders_filter: Some(OrdersFilter {
-            subaccount_ids: vec!["*".to_string()],
-            market_ids: vec!["*".to_string()],
-        }),
-        derivative_orders_filter: Some(OrdersFilter {
-            subaccount_ids: vec!["*".to_string()],
-            market_ids: vec!["*".to_string()],
-        }),
-        positions_filter: Some(PositionsFilter {
-            subaccount_ids: vec!["*".to_string()],
-            market_ids: vec!["*".to_string()],
-        }),
-        oracle_price_filter: Some(OraclePriceFilter {
-            symbol: vec!["*".to_string()],
-        }),
-        derivative_orderbooks_filter: Some(OrderbookFilter {
-            market_ids: vec!["*".to_string()],
-        }),
-        spot_orderbooks_filter: Some(OrderbookFilter {
-            market_ids: vec!["*".to_string()],
-        }),
-        spot_trades_filter: Some(TradesFilter {
-            subaccount_ids: vec!["*".to_string()],
-            market_ids: vec!["*".to_string()],
-        }),
-        derivative_trades_filter: Some(TradesFilter {
-            subaccount_ids: vec!["*".to_string()],
-            market_ids: vec!["*".to_string()],
-        }),
-    });
+
+    let req = Request::new(create_stream_request(StreamFilterCondition::DerivativeTrades)?);
     let mut stream = client.stream(req).await?.into_inner();
-    while  let Some(reply) = stream.next().await {
+    while let Some(reply) = stream.next().await {
         match reply {
-            Ok(message) => println!("received stream message : {:?}", message),
+            Ok(message) => println!("received stream message : {:?}", message.derivative_trades),
             Err(e) => {
-                println!(
-                    "error in msg"
-                );
+                println!("error in msg");
                 break;
             }
         }
